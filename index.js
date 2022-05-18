235efb83c42fd77c9c005869bf267ca3e4a4a99e
CarlEverest78 <thepythoneer@gmail.com>
2022-05-18 18:11:00 +0200

    final commit for May 18th

--- index.js
+++ index.js
@@ -28,43 +28,53 @@ invoiceBtn.addEventListener("click", () => location.reload())
 for (let i=0; i<customBtns.length; i++) {
     customBtns[i].addEventListener("click", function(){
         let taskId = customBtns[i].id
-        if (!tasksDone.includes(tasks[taskId].name)) {
-            tasksDone.push(tasks[taskId].name)
+        if (!tasksDone.includes(taskId)) {
+            tasksDone.push(taskId)
             priceList.push(tasks[taskId].price)
             acceptText.style.visibility = "visible"
+            customBtns[i].style.display = "none"
             renderTaskList()
         }
     })
 }
 
+
 function renderTaskList() {
     taskListEl.textContent = ""
     priceListEl.textContent = ""
     for (let i = 0; i < tasksDone.length;i++) {
-        if (tasksDone[i]) {
-            let newTask = document.createElement("li")
-            newTask.textContent = tasksDone[i]
+        let listItem = document.createElement("li")
+        let newTask = document.createElement("p")
+        let taskId = tasksDone[i]
+        let taskName = tasks[taskId].name
+        newTask.textContent = taskName 
+        let removeBtn = document.createElement("button")
+        removeBtn.textContent = "Remove"
+        removeBtn.setAttribute("class", "remove-btn")
+        removeBtn.onclick = function() {
+            tasksDone.splice(i, 1)
+            priceList.splice(i, 1)
+            let addButton = document.getElementById(taskId)
+            addButton.style.display = "inline"
+            renderTaskList()}
+        
+        taskListEl.append(listItem)
+        listItem.append(newTask)
+        listItem.append(removeBtn)
 
-            let removeBtn = document.createElement("button")
-            removeBtn.textContent = "Remove"
-            removeBtn.onclick = function() {
-                tasksDone.splice(i, 1)
-                priceList.splice(i, 1)
-                renderTaskList()}
-                
-            taskListEl.append(removeBtn)
-            taskListEl.append(newTask)
-
-            let newPrice = document.createElement("li")
-            newPrice.innerHTML = `<p><span class="dollar-sign">$ </span>${priceList[i]}</p>`
-            priceListEl.append(newPrice)
-
-
-
-        }
+        let newPrice = document.createElement("li")
+        newPrice.innerHTML = `<p><span class="dollar-sign">$ </span>${priceList[i]}</p>`
+        priceListEl.append(newPrice)
     }
 
-    let totalPrice = priceList.reduce(function(previousVal, currentVal) {return previousVal + currentVal})
+    let totalPrice = 0
+
+    if (priceList.length >= 1) {
+        totalPrice = priceList.reduce(function(previousVal, currentVal) {return previousVal + currentVal})
+    } else {
+        location.reload()
+    }
+
     totalAmount.textContent = "$ " + totalPrice
     totalAmount.style.color = "#4A4E74"
 }
